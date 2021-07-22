dadc128d953f7399a108319a130702fb9acca4b8
diego <diego.y8@gmail.com>
2021-07-22 11:15:11 -0400

    ConnectMongoDB

---
+++ controllers/transactions.js
@@ -0,0 +1,22 @@
+//Get all transactions
+//GET /api/v1/transactions
+// public
+
+exports.getTransactions = (req,res,next) =>{
+    res.send('GET transactions');
+}
+
+//Add transactions
+//POST /api/v1/transactions
+// public
+
+exports.addTransactions = (req,res,next) =>{
+    res.send('POST transactions');
+}
+//Delete transactions
+//DELETE /api/v1/transactions
+// public
+
+exports.deleteTransactions = (req,res,next) =>{
+    res.send('DELETE transactions');
+}
